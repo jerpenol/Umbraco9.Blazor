fc77fc5472a659fc6859843bee782b7f78d0761b
Jeroen Koppenol <j.koppenol@arlanet.com>
2021-07-09 10:24:35 +0200

    Added code-behind file to Alerts component (Code behind)

---
+++ Src/Umbraco9.Blazor.Web/Shared/Alerts.razor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Umbraco9.Blazor.Web.App_Code;
+
+namespace Umbraco9.Blazor.Web.Shared
+{
+    public partial class Alerts
+    {
+        public List<AlertModel> ActiveAlerts { get; set; }
+
+        protected override void OnInitialized()
+        {
+            ActiveAlerts = new List<AlertModel>
+            {
+                new AlertModel
+                {
+                    Title = "Umbraco ❤ Blazor",
+                    Text = "Initial alert",
+                    Type = "info"
+                }
+            };
+        }
+
+        private void CloseAlert(AlertModel alert)
+        {
+            ActiveAlerts.Remove(alert);
+        }
+
+        private void FetchAlert()
+        {
+            ActiveAlerts.Add(new AlertModel
+            {
+                Title = "Umbraco ❤ Blazor",
+                Text = "A new alert!",
+                Type = "success"
+            });
+        }
+    }
+}
