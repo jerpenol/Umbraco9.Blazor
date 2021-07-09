f084481b01cd17eedf3dca6bfa6ca2e9ebfc68f9
Jeroen Koppenol <j.koppenol@arlanet.com>
2021-07-09 10:28:15 +0200

    Added Parameter to Alerts component

--- Src/Umbraco9.Blazor.Web/Shared/Alerts.razor.cs
+++ Src/Umbraco9.Blazor.Web/Shared/Alerts.razor.cs
@@ -1,10 +1,12 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
 using Umbraco9.Blazor.Web.App_Code;
 
 namespace Umbraco9.Blazor.Web.Shared
 {
     public partial class Alerts
     {
+        [Parameter] public int? MaximumNumberOfAlerts { get; set; }
         public List<AlertModel> ActiveAlerts { get; set; }
 
         protected override void OnInitialized()
@@ -27,6 +29,11 @@ namespace Umbraco9.Blazor.Web.Shared
 
         private void FetchAlert()
         {
+            if(MaximumNumberOfAlerts.HasValue && ActiveAlerts.Count >= MaximumNumberOfAlerts.Value)
+            {
+                ActiveAlerts.RemoveAt(0);
+            }
+
             ActiveAlerts.Add(new AlertModel
             {
                 Title = "Umbraco ❤ Blazor",
