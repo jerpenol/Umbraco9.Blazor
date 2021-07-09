f30437573d9448160d50ee1fec6b895f815d4163
Jeroen Koppenol <j.koppenol@arlanet.com>
2021-07-09 10:22:32 +0200

    Added Alerts Component (Code in view)

---
+++ Src/Umbraco9.Blazor.Web/App_Code/AlertModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Umbraco9.Blazor.Web.App_Code
+{
+    public class AlertModel
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Type { get; set; }
+    }
+}
